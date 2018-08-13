7cc7f37b9b83a7939f934b7fd01448cfc52f77d9
Dan Anderson <anderson@familymedia.org>
2018-08-13 14:32:22 -0400

    Remove Themes Contact Form

--- public/assets/php/c_config.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-    define('XPEEDSTUDIO_EMAIL', 'xpeedstudioteam@gmail.com');
-    define('XPEEDSTUDIO_SUBJECT', 'New Contact Email');
-    define('XPEEDSTUDIO_SUCCESS_MASSAGE', 'Successfully Sent Email');
-
-?>
