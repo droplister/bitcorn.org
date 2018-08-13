fc93b28869a3b69bf6cda78def529078654e5b3a
Dan Anderson <anderson@familymedia.org>
2018-08-13 19:05:09 -0400

    Update Causes

--- resources/views/causes/partials/categories.blade.php
+++ resources/views/causes/partials/categories.blade.php
@@ -3,7 +3,6 @@
         <div class="row">
             <div class="col-md-6 col-lg-4">
                 <div class="xs-single-causes">
-                    <img src="assets/images/causes/causes_1.jpg" alt="">
                     <div class="xs-causes-footer">
                         <h2 class="color-light-red">Memes</h2>
                         <p>We support the creation of memes and memetic projects beyond the scope of Bitcorn cards, if they are worthy and good natured.</p>
@@ -12,7 +11,6 @@
             </div>
             <div class="col-md-6 col-lg-4">
                 <div class="xs-single-causes">
-                    <img src="assets/images/causes/causes_2.jpg" alt="">
                     <div class="xs-causes-footer">
                         <h2 class="color-purple">Community</h2>
                         <p>Efforts that raise up or support the bitcorn farmer community, either as a whole or individually can be submitted as causes.</p>
@@ -21,7 +19,6 @@
             </div>
             <div class="col-md-6 col-lg-4">
                 <div class="xs-single-causes">
-                    <img src="assets/images/causes/causes_3.jpg" alt="">
                     <div class="xs-causes-footer">
                         <h2 class="color-blue">Development</h2>
                         <p>If you are a developer you can raise funds for development, especially Bitcorn development, but also Counterparty generally.</p>
