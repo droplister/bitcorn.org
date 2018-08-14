70fd93367f65b1f3bc462574859d59032c077290
Dan Anderson <anderson@familymedia.org>
2018-08-14 01:46:16 -0400

    Update Auth

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -3,18 +3,25 @@
 @section('title', 'Reset Password')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('Reset Password') }}</div>
 
-                <div class="card-body">
-                    @include('partials.session')
-                    @include('auth.forms.email')
+                    <div class="card-body">
+                        @include('partials.session')
+                        @include('auth.forms.email')
+                    </div>
                 </div>
+                <p class="text-muted text-center py-4 mb-0">
+                    Don't have an account?
+                    <a href="{{ route('register') }}" class="ml-1">
+                        <i class="fa fa-user"></i>
+                        Register
+                    </a>
+                </p>
             </div>
         </div>
     </div>
-</div>
 @endsection
--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -13,13 +13,6 @@
                         @include('auth.forms.reset')
                     </div>
                 </div>
-                <p class="text-muted text-center py-4 mb-0">
-                    Don't have an account?
-                    <a href="{{ route('register') }}" class="ml-1">
-                        <i class="fa fa-user"></i>
-                        Register
-                    </a>
-                </p>
             </div>
         </div>
     </div>
