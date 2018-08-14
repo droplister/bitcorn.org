42fb26cd05040f0dbe952927f66f4d1ab842e519
Dan Anderson <anderson@familymedia.org>
2018-08-14 18:47:51 -0400

    Update Content

--- resources/views/causes/partials/content.blade.php
+++ resources/views/causes/partials/content.blade.php
@@ -2,10 +2,11 @@
     <div class="post-body xs-border xs-padding-40">
         <div class="entry-content">
             <h1>{{ $cause->name }}</h1>
-            <p>By: {{ $cause->user->name }} - Posted: {{ $cause->approved_at->format('F jS, Y') }}.</p>
+            <p>By: {{ $cause->user->name }} - Posted: {{ $cause->approved_at ? $cause->approved_at->format('F jS, Y') : 'Not Published' }}.</p>
             <img src="{{ $cause->image_url }}" class="float-left mb-4 mr-4 d-none d-sm-inline-block" />
             <img src="{{ $cause->image_url }}" class="mb-4 d-block d-sm-none" />
             @markdown($cause->content)
+            <div class="clearfix"></div>
         </div>
     </div>
 </article>
