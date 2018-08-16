36453b5d8f5ed6e4d7e13649d2eac050c6e74a02
Dan Anderson <anderson@familymedia.org>
2018-08-15 22:17:47 -0400

    Turn off command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('update:index')->everyMinute();
+        // $schedule->command('update:index')->everyMinute();
     }
 
     /**
