5d56c9f909a7266c7871da5980f9fc833cd25a82
Dan Anderson <anderson@familymedia.org>
2018-08-18 03:18:42 -0400

    Update Asterisks

--- resources/views/dashboard/partials/form.blade.php
+++ resources/views/dashboard/partials/form.blade.php
@@ -3,7 +3,7 @@
     @csrf
 
     <div class="form-group row">
-        <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+        <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }} <span class="text-danger">*</span></label>
 
         <div class="col-md-6">
             <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') ? old('name') : Auth::user()->name }}" required>
@@ -17,7 +17,7 @@
     </div>
 
     <div class="form-group row">
-        <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail') }}</label>
+        <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail') }} <span class="text-danger">*</span></label>
 
         <div class="col-md-6">
             <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') ? old('email') : Auth::user()->email }}" required>
